31932c6b50ca2928d5f7b95965268bb9137e25bf
Eric Gobbo <33002736+Ericgobbo@users.noreply.github.com>
2021-07-23 02:09:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function validateEmail(email) {
+  const re = /.+\@.+\..+/;
+  return re.test(email);
+}
+
+const inpgroup = document.getElementById("inp-group");
+const textinput = document.getElementById("email");
+const warningElements = document.querySelectorAll(".warning");
+
+function validation() {
+  validEmail = validateEmail(textinput.value);
+
+  if (!validEmail) {
+    inpgroup.classList.add("not-valid");
+    for (let i = 0; i < 2; i++) {
+      warningElements[i].style.display = "block";
+    }
+    // warningElements.style.display = "block";
+  } else {
+    inpgroup.classList.remove("not-valid");
+    for (let i = 0; i < 2; i++) {
+      warningElements[i].style.display = "none";
+    }
+  }
+}
